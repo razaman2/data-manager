bfe2c031b581665d40a4a5b99e54c5c31fce9fd1
razaman2 <razamvmt@gmail.com>
2021-12-12 05:44:26 -0500

    rebuilt project files

--- lib/DataClient.d.ts
+++ lib/DataClient.d.ts
@@ -3,8 +3,8 @@ export default interface DataClient extends Record<string, any> {
     logging?: boolean;
     model?: DataClient;
     data?: (() => Record<string, any>) | Record<string, any>;
+    getIgnoredKeys?: (keys: Array<string>) => Array<string>;
     getDefaultData?: () => Record<string, any>;
     getNotifications?: () => EventEmitter;
-    getIgnoredKeys?: (keys: Array<string>) => Array<string>;
 }
 //# sourceMappingURL=DataClient.d.ts.map
