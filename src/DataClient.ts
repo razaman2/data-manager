455319cb76a325cd070e5149df4a17d48b3a4126
razaman2 <razamvmt@gmail.com>
2023-03-30 11:01:56 -0400

    fix datatype to require an optional value

--- src/DataClient.ts
+++ src/DataClient.ts
@@ -5,9 +5,7 @@ type Datatype = Record<string, any> | Array<any>;
 export default interface DataClient extends Record<string, any> {
     logging?: boolean;
     model?: DataClient;
-    data?: {
-        value?: (() => Datatype) | Datatype
-    };
+    data?: (() => Datatype) | Datatype;
     defaultData?: (() => Datatype) | Datatype;
     ignoredKeys?: (keys: Array<string>) => typeof keys;
     notifications?: EventEmitter;
